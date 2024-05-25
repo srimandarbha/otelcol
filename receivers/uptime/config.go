e2159f517c84557df16b43a996b55498ad833b72
sriman <sriman1988@gmail.com>
2024-05-25 20:48:47 +0530

    refactor

--- receivers/uptime/config.go
+++ receivers/uptime/config.go
@@ -1,1 +1,10 @@
-//
+package uptime
+
+import (
+	"go.opentelemetry.io/collector/receiver/scraperhelper"
+)
+
+type Config struct {
+	// ScraperControllerSettings to configure scraping interval (default: scrape every second)
+	scraperhelper.ScraperControllerOption `mapstructure:",squash"`
+}
--- receivers/uptime/factory.go
+++ receivers/uptime/factory.go
@@ -1,1 +1,59 @@
-//
+package uptime
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/srimandarbha/otelcol/receivers/uptime/internal/metadata"
+	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/consumer"
+	"go.opentelemetry.io/collector/receiver"
+	"go.opentelemetry.io/collector/receiver/scraperhelper"
+	"go.uber.org/zap"
+)
+
+func NewFactory() receiver.Factory {
+	return receiver.NewFactory(
+		metadata.Type,
+		createDefaultConfig,
+		receiver.WithMetrics(CreateVmStatReceiver, component.StabilityLevelDevelopment),
+	)
+}
+
+func createDefaultConfig() component.Config {
+	return &Config{
+		Delay: 1,
+		Count: 1,
+	}
+}
+
+func CreateVmStatReceiver(
+	_ context.Context,
+	settings receiver.CreateSettings,
+	cfg component.Config,
+	consumer consumer.Metrics,
+) (receiver.Metrics, error) {
+	logger := settings.Logger
+	config, ok := cfg.(*Config)
+	if !ok {
+		em := "failed to cast to type Config"
+		logger.Error(em)
+		return nil, fmt.Errorf(em)
+	}
+
+	mb := metadata.NewMetricsBuilder(config.MetricsBuilderConfig, settings)
+
+	ns := newScraper(config, mb, logger)
+	scraper, err := scraperhelper.NewScraper(metadata.Type, ns.scrape)
+	if err != nil {
+		logger.Error("failed to create scraper", zap.Error(err))
+		return nil, err
+	}
+
+	return scraperhelper.NewScraperControllerReceiver(
+		&config.ScraperControllerSettings,
+		settings,
+		consumer,
+		scraperhelper.AddScraper(scraper),
+	)
+}
